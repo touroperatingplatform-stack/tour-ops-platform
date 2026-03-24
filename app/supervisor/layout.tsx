cdd6e66d69ec03b4d31529d18e4ac2968459b771
projectmadacc-cell <projectmadacc@gmail.com>
2026-03-24 11:27:44 -0600

    Fix padding clipping - move p-3 inside scroll area to avoid overflow-hidden conflict

--- app/supervisor/layout.tsx
+++ app/supervisor/layout.tsx
@@ -97,9 +97,11 @@ export default function SupervisorLayout({
             </div>
           </header>
 
-          {/* Page Content - TEST: p-6 to verify padding works */}
-          <main className="flex-1 p-6 overflow-y-auto">
-            {children}
+          {/* Page Content - padding inside scroll area to avoid overflow-hidden clipping */}
+          <main className="flex-1 overflow-y-auto">
+            <div className="p-3">
+              {children}
+            </div>
           </main>
         </div>
       </div>
